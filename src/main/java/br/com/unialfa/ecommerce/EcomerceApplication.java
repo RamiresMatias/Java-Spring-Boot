5a6d2f2a472762712c2df0208fd65717d600fe85
RamiresMatias <ramiresmatias20000@gmail.com>
2021-03-26 18:44:14 -0300

    Projeto com spring boot

--- src/main/java/br/com/unialfa/ecommerce/EcomerceApplication.java
+++ src/main/java/br/com/unialfa/ecommerce/EcomerceApplication.java
@@ -47,7 +47,6 @@ public class EcomerceApplication {
 		cliente.setPrimeiroNome("Teste");
 		clienteRepository.save(cliente);
 
-
 		Pedido pedido = new Pedido();
 		pedido.setCliente(cliente);
 		pedido.setDataCriacaoPedido(LocalDate.now());
